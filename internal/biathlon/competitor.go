606662eb511a3620daa611df1316f524276b5012
Chernovuk <cherpash20@mail.ru>
2025-05-07 06:25:53 +0300

    fix(biathlon): Remove unnecessary fields and struct.

--- internal/biathlon/competitor.go
+++ internal/biathlon/competitor.go
@@ -2,20 +2,6 @@ package biathlon
 
 import "time"
 
-type LapInfo struct {
-	StartTime time.Time
-	EndTime   time.Time
-	Duration  time.Duration
-	AvgSpeed  float64
-}
-
-type PenaltyLapInfo struct {
-	EntryTime time.Time
-	ExitTime  time.Time
-	Duration  time.Duration
-	AvgSpeed  float64
-}
-
 type competitorStatus int
 
 const (
@@ -32,28 +18,10 @@ const (
 	CannotContinue
 )
 
-type Competitor struct {
-	ID                 int
-	Status             competitorStatus
-	ScheduledStartTime time.Time
-	ActualStartTime    time.Time
-	FinishTime         time.Time
-	CurrentLap         int
-	TotalHits          int
-	TotalShots         int
-	LapsInfo           []LapInfo
-	PenaltiesInfo      []PenaltyLapInfo
-	DNFComment         string
-	LastEventTime      time.Time
-	RangeEntryTime     time.Time
-	PenaltyEntryTime   time.Time
-	HitsThisRange      [5]bool
-	VisitedRanges      []bool
-}
-
 type CompetitorState struct {
 	ID                 int
 	Status             competitorStatus
+	CurrentLap         int
 	ScheduledStartTime time.Time
 	ActualStartTime    time.Time
 	VisitedRanges      []bool
