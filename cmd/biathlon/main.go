5d385326f72ac0288e7d633a76b29952797c3272
Chernovuk <cherpash20@mail.ru>
2025-05-07 08:01:34 +0300

    refactor(main): Set log files and refactor.

--- cmd/biathlon/main.go
+++ cmd/biathlon/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/Chernovuk/biathlon-competetions/internal/biathlon"
@@ -10,27 +11,54 @@ import (
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %v EVENTS_FILEPATH CONFIG_FILEPATH", os.Args[0])
+		fmt.Printf("Usage: %v EVENTS_FILEPATH CONFIG_FILEPATH\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	eventsFP := os.Args[1]
 	listener, err := biathlon.NewEventListener(os.Args[1])
 	if err != nil {
-		fmt.Printf("Failed to open specified file: %v", eventsFP)
+		fmt.Printf("Failed to open specified file: %v\n", eventsFP)
 		os.Exit(1)
 	}
+
+	listenerLogFile, err := os.OpenFile("listener.log", os.O_WRONLY|os.O_CREATE, 0o644)
+	if err != nil {
+		fmt.Printf("Failed to open listne log file: %v\n", listenerLogFile)
+		os.Exit(1)
+	}
+	defer listenerLogFile.Close()
+
+	listener.SetLogger(log.New(listenerLogFile, "Listener: ", log.Ltime))
 
 	configFP := os.Args[2]
 	config, err := biathlon.ParseConfig(configFP)
 	if err != nil {
-		fmt.Printf("Failed to open specified file: %v", configFP)
+		fmt.Printf("Failed to open specified file: %v\n", configFP)
 		os.Exit(1)
 	}
 
 	stats := statistics.New(config)
 	processor := biathlon.NewProcessor(config, listener.Events())
+	handleStats(processor, stats)
 
+	processorLogFile, err := os.OpenFile("processor.log", os.O_WRONLY|os.O_CREATE, 0o644)
+	if err != nil {
+		fmt.Printf("Failed to open listne log file: %v\n", processorLogFile)
+		os.Exit(1)
+	}
+	defer processorLogFile.Close()
+
+	processor.SetLogger(biathlon.NewDefaultLogger(processorLogFile))
+
+	go listener.Start()
+	processor.Start()
+
+	table := stats.GetResults()
+	showReport(table)
+}
+
+func handleStats(processor *biathlon.Processor, stats *statistics.Statistics) {
 	processor.Handle(biathlon.Register, stats.OnRegister)
 	processor.Handle(biathlon.BeSheduled, stats.OnBeSheduled)
 	processor.Handle(biathlon.Start, stats.OnStart)
@@ -42,12 +70,9 @@ func main() {
 	processor.Handle(biathlon.BeUnableToContinue, stats.OnBeUnableToContinue)
 	processor.Handle(biathlon.Disqualify, stats.OnDisqualify)
 	processor.Handle(biathlon.Finish, stats.OnFinish)
+}
 
-	go listener.Start()
-	processor.Start()
-
-	fmt.Println()
-	table := stats.GetResults()
+func showReport(table []statistics.Result) {
 	for _, v := range table {
 		fmt.Println(v.String())
 	}
